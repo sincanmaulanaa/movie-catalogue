398f1d3d6c3f86f7dbe3af8211e643d3bfe205dc
Sincan Maulana <sincanmaulanaa@gmail.com>
2022-12-17 18:47:04 +0800

    create config object to store API informations

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '33a9bd60c1dfc438942bec0a58378d26',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
