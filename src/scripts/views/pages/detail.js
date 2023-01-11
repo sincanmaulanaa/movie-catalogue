ac906329bbaf6a46bd6a2ad906a4fdbcc8c70e94
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-12 01:03:56 +0700

    add like button initiator object into detail page

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-tabs */
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parser';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 import { createMovieDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
 		<div id="movie" class="movie"></div>
+    <div id="likeButtonContainer"></div>
 	`;
   },
 
@@ -14,7 +16,19 @@ const Detail = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const movie = await TheMovieDbSource.detailMovie(url.id);
     const movieContainer = document.querySelector('#movie');
+
     movieContainer.innerHTML = createMovieDetailTemplate(movie);
+
+    LikeButtonInitiator.init({
+      LikeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average,
+      },
+    });
   },
 };
 
