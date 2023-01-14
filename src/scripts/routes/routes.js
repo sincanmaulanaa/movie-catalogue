d390d6cf5048fdbf9ad21ee3d91edc275ea794e4
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-14 22:16:25 +0700

    create Like as a new route

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,14 @@
 import NowPlaying from '../views/pages/now-playing';
 import Detail from '../views/pages/detail';
 import Upcoming from '../views/pages/upcoming';
+import Like from '../views/pages/like';
 
 const routes = {
   '/': NowPlaying, // default page
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
   '/detail/:id': Detail,
+  '/like': Like,
 };
 
 export default routes;
